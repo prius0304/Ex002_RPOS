6474434d86044a832e303e6eeeb813fbfd839583
Peter·Shen <2639035034@qq.com>
2017-05-08 22:52:21 +0800

    CLI

---
+++ UDP_Socket/UDP_Socket/CLI.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDP_Socket
+{
+    public class CLI
+    {
+        int cmd_num_current = 0;
+
+		struct CMD_List
+		{
+			public byte[] name;
+			public string usage;
+			public delegate void DelegateMethod(byte[] argv);
+			public DelegateMethod delegateMethod;
+
+			public void RunDelegateMethods(byte[] argv)
+			{
+				if (delegateMethod != null)
+					delegateMethod.Invoke(argv);
+			}
+		}
+
+        CMD_List[] cmd_list = new CMD_List[50];
+
+		int Subyte(byte[] new_arr, byte[] raw_arr, int a, int b)
+		{
+			if (raw_arr.Length >= b)
+			{
+				for (int i = a; i<b; i++)
+					new_arr[i-a] = raw_arr[i];
+				return 0;
+			}
+			else
+			{
+				Console.WriteLine("Error!\n");
+				return -1;
+			}
+		}
+
+		private void CMD_Register(byte[] name, string usage, CMD_List.DelegateMethod method)
+		{
+            int ret;
+
+            Subyte(cmd_list[cmd_num_current].name, name, 0, name.Length);
+            cmd_list[cmd_num_current].usage = usage;
+            cmd_list[cmd_num_current].delegateMethod = method;
+
+            cmd_num_current++;
+		}
+
+		public CLI()
+		{
+            byte[] rpos = new byte[] { 0x52, 0x50, 0x4F, 0x53 };
+            CMD_Register(rpos, "RPOS", RPOS);
+		}
+        public void command(byte[] args)
+        {
+            byte[] Command_name = new byte[4];
+            byte[] Command_value = new byte[514];
+
+            if (args.Length > 5)
+            {
+                Subyte(Command_name, args, 0, 3);
+                Subyte(Command_value, args, 5, args.Length);
+            }
+            else
+            {
+                Subyte(Command_name, args, 0, 3);
+                Subyte(Command_value, args, 4, 4);
+            }
+
+            for (int i = 0; i < Command_name.Length; i++)
+                Console.Write(Command_name[i] + " ");
+            Console.WriteLine();
+            for (int i = 0; i < Command_value.Length; i++)
+                Console.Write(Command_value[i] + " ");
+            Console.WriteLine();
+        }
+
+
+        public void RPOS(byte[] argv)
+        {
+            Console.WriteLine("YES");
+        }
+    }
+}
--- UDP_Socket/UDP_Socket/Program.cs
+++ UDP_Socket/UDP_Socket/Program.cs
@@ -13,23 +13,16 @@ namespace UDP_Socket
     class Program
     {
         static Socket server;
+		static CLI cli = new CLI();
+        static string IP_ADD = "127.0.0.1";
+        static int Loc_Port = 56833;
+        static int Des_Port = 49000;
+
         static byte[] RPOS = new byte[7] { 0x52, 0x50, 0x4F, 0x53, 0x00, 0x31, 0x00 };
-        static void Main(string[] args)
-        {
-            server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            server.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 56833));
-            Console.WriteLine("服务端开启！");
-
-            Thread t1 = new Thread(ReciveMsg);
-            t1.Start();
-
-            Thread t2 = new Thread(sendMsg);
-            t2.Start();
-        }
 
         static void sendMsg()
         {
-            EndPoint point = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 49000);
+            EndPoint point = new IPEndPoint(IPAddress.Parse(IP_ADD), Des_Port);
             while (true)
             {
                 string msg = Console.ReadLine();
@@ -48,11 +41,26 @@ namespace UDP_Socket
                 //string message = Encoding.UTF8.GetString(buffer, 0, length);
                 //Console.WriteLine(point.ToString() + "   " + message);
                 Console.Write(point.ToString() + "   ");
-                for(int i=0;i<buffer.Length;i++)
+                for (int i = 0; i < buffer.Length; i++)
                     Console.Write(buffer[i] + "   ");
                 Console.WriteLine();
                 Console.WriteLine();
             }
+        }
+
+        static void Main(string[] args)
+        {
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            server.Bind(new IPEndPoint(IPAddress.Parse(IP_ADD), Loc_Port));
+            Console.WriteLine("服务端开启！");
+
+			cli.command(RPOS);
+
+            /*Thread t1 = new Thread(ReciveMsg);
+            t1.Start();
+
+            Thread t2 = new Thread(sendMsg);
+            t2.Start();*/
         }
     }
 }
